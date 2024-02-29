05e3b589c4f7de646fe852b139c0a52fe1622b48
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2024-03-01 00:26:59 +0700

    Add files via upload

---
+++ RMSNorm.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+
+
+class RMSNorm(nn.Module):
+    def __int__(self, dim, eps: float):
+        self.weight = nn.Parameter(torch.ones(dim))  # Use this nn.Parameter to assign
+        # (tell model that this weight is learnable)
+        self.eps = eps
+
+    def forward(self, inputs):
+        hidden = inputs.to(torch.float32)
+        variance = torch.pow(hidden, 2).mean(dim=-1)
+        hidden = hidden * torch.rsqrt(variance + self.eps)
+        return (self.weight * hidden).to(inputs.dtype)
