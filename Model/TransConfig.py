d94e3c420fcf45e050d18cb299754c638fbdef2d
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2024-03-09 21:18:45 +0700

    Add files via upload

---
+++ Model/TransConfig.py
@@ -0,0 +1,30 @@
+class TransformersConfig:
+    def __init__(self,
+                 hidden_dim=512,
+                 head_num=8,
+                 dropout=0.1,
+                 padding_idx=1,
+                 max_len=2048,
+                 vocab_size=32000,
+                 ffn_dim=1792,
+                 attn="base",
+                 ffn_dropout=0,
+                 ffn_bias=False,
+                 expert_num=8,
+                 top_k=2,
+                 eps=1e-6):
+        self.hidden_dim = hidden_dim
+        self.head_num = head_num
+        self.dropout = dropout
+        self.padding_idx = padding_idx
+        self.max_len = max_len
+        self.vocab_size = vocab_size
+        self.attn = attn
+        self.ffn_dim = ffn_dim
+        self.ffn_bias = ffn_bias
+        self.ffn_dropout = ffn_dropout
+        self.expert_num = expert_num
+        self.top_k = top_k
+        self.eps = eps
+
+
